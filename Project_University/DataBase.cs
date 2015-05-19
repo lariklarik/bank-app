dfaf534c4b728f2c0c39b6304ce62f5cd1a268ae
madputin <anatoly.gulyaev@gmail.com>
2015-05-19 22:49:49 +0900

    DataBase.cs class is added

---
+++ Project_University/DataBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Project_University
+{
+    class DataBase
+    {
+        MySqlConnectionStringBuilder mysqlCSB;
+
+        public DataBase()
+        {
+
+        }
+    }
+}
