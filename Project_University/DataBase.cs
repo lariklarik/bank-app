fc0e30950e9bd9fad2f79bfa7e31c66f770aef60
madputin <anatoly.gulyaev@gmail.com>
2015-05-19 23:03:33 +0900

    Updated DataBase.cs

--- Project_University/DataBase.cs
+++ Project_University/DataBase.cs
@@ -2,18 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
+using MySql;
 
 namespace Project_University
 {
-    class DataBase
+    static class DataBase
     {
-        MySqlConnectionStringBuilder mysqlCSB;
-
-        public DataBase()
+        static private void CreateConnection()
         {
-
+            MySqlConnectionStringBuilder mysqlCSB = new MySqlConnectionStringBuilder();
+            mysqlCSB.Server = "127.0.0.1";
+            mysqlCSB.Database = "world";
+            mysqlCSB.UserID = "root";
+            mysqlCSB.Password = "123456";
         }
     }
 }
