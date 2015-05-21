178ba45dce247e71e5bc4be2eb746ce330e5bd63
lariklarik <larianantonova@mail.ru>
2015-05-21 13:55:23 +0400

    Update Account.cs

--- Project_University/Account.cs
+++ Project_University/Account.cs
@@ -1,14 +1,30 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Project_University
+namespace Project
 {
-    class Account
+    class Accounts
     {
-        int id;
+        Date openingDate;
         string number;
+        string currency;   //валюта
+        double balance;
+        bool isOpened;
+        bool isBlocked;
+        string accountType;
+
+        public Accounts(Date _openingDate, string _number, string _currency, double _balance, bool _isOpend, bool _isBlocked, string _accountType) 
+        {
+            openingDate = _openingDate;
+            number = _number;
+            currency = _currency;
+            balance = _balance;
+            isOpened = _isOpend;
+            isBlocked = _isBlocked;
+            accountType = _accountType;
+        } 
     }
 }
